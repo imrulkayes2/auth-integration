81fd82622ae7819daacec8eb8025e692c630de3e
imrulkayes75 <ikbubt@gmail.com>
2024-01-20 00:27:55 +0600

    Introduction To Private Route And Handle Loading State

--- src/MyCreateRoute/MyCreateRoute.jsx
+++ src/MyCreateRoute/MyCreateRoute.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../MainLayout/MainLayout";
 import Home from "../Pages/Home/Home";
 import LogIn from "../Pages/LogIn/LogIn";
 import Register from "../Pages/Register/Register";
+import Orders from "../Components/Orders/Orders";
+import PrivetRoute from "../Route/PrivetRoute/PrivetRoute";
 
 
 const MyCreateRoute = createBrowserRouter([
@@ -21,6 +23,10 @@ const MyCreateRoute = createBrowserRouter([
             {
                 path: '/register',
                 element: <Register></Register>
+            },
+            {
+                path: '/orders',
+                element: <PrivetRoute><Orders></Orders></PrivetRoute>
             }
         ]
     }
